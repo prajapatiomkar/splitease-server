c96e307149cb4e36a7f57c7173c21922b4126675
Omkar Prajapati <prajapatiomkar9833@gmail.com>
2025-02-28 11:36:33 +0530

    Added the middleware and the CORS.

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,28 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes";
 import userRoutes from "./routes/user.routes";
+import cors from "cors";
 
 const app = express();
 
-// Middleware
+// CORS Middleware
+app.use(
+  cors({
+    origin: "http://localhost:5000", // Change this to your frontend URL
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
+
+// Handle Preflight Requests
+app.options("*", cors());
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
+
 export default app;
